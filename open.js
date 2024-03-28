e78aeaf7824fbd07d1526bcac242e448e889ba3b
shrutinatala <161991409+shrutinatala@users.noreply.github.com>
2024-03-27 22:40:33 -0700

    Update open.js to adjust for Daylight Savings

--- open.js
+++ open.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     var statusElement = document.getElementById('status');
     var now = new Date();
+    var month = now.getUTCMonth() + 1; // Months are zero indexed, so add 1
+    var day = now.getUTCDate();
     var currentUTCHour = now.getUTCHours();
-    var currentPSTHour = (currentUTCHour - 8 + 24) % 24; // Adjusting for PST
+    var currentPSTHour;
+
+    // Check if current date is within DST period (March to November)
+    if ((month > 3 && month < 11) || (month === 3 && day > 7) || (month === 11 && day < 7)) {
+        currentPSTHour = (currentUTCHour - 7 + 24) % 24; // Adjusting for PDT
+    } else {
+        currentPSTHour = (currentUTCHour - 8 + 24) % 24; // Adjusting for PST
+    }
 
     if (currentPSTHour >= 11 && currentPSTHour < 22) {
         statusElement.textContent = "open 🥳";
